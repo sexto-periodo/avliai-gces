c351c1288dea782cebcfa121102cc41cea49ec81
Luiz Gustavo Santos Soares <luiz.soares.1310151@sga.pucminas.br>
2023-03-28 15:43:21 -0300

    Base para desenvolvimento do backend
    Ainda é necessário colocar o backend em docker e realizar a conexão com o banco de dados.

---
+++ codigo/AvaliAi/src/main/java/AvaliAi/AvaliAiApplication.java
@@ -0,0 +1,13 @@
+package AvaliAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliAiApplication.class, args);
+	}
+
+}
