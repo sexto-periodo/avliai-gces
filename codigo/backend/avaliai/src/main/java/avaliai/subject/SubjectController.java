176628bceb9e049fd1de0e2e7b613496789e5999
Luiz Gustavo Santos Soares <luiz.soares.1310151@sga.pucminas.br>
2023-04-05 14:34:51 -0300

    [BUG FIX] [BACK-END] Correção de problemas na conexão do backend com o banco de dados

--- codigo/backend/avaliai/src/main/java/avaliai/subject/SubjectController.java
+++ codigo/backend/avaliai/src/main/java/avaliai/subject/SubjectController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/v1/subject")
 public class SubjectController {
